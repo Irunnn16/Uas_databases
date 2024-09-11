4d93fb484076367c93ee53f2639074999547621b
Irunnn16 <khoirunnugraha145@gmail.com>
2024-09-11 19:46:43 +0800

    fitst commit

---
+++ database_schema.sql
@@ -0,0 +1,14 @@
+create database uas_database;
+use uas_database;
+
+create user(
+    id int primary key auto_increment,
+    nama varchar(50) not null,
+    telepon varchar(13) not null,
+);
+create table post(
+    id int primary key auto_increment,
+    judul varchar(50) not null,
+    tulisan varchar(13) not null,
+    user_id int,
+);
